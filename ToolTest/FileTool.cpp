76dd071e9e0de6614ead0ef4b8b6a50e8b04845b
xtayaitak <zds1275131667@gmail.com>
2018-03-16 17:46:01 +0800

    333

--- ToolTest/FileTool.cpp
+++ ToolTest/FileTool.cpp
@@ -15,15 +15,11 @@
 #include "DebugOutput.h"
 #include <mutex>
 
-<<<<<<< HEAD
+
 #include "ResManager.h"
 
 #include <tchar.h>
 
-
-=======
-#include "CLResManager.h"
->>>>>>> ed3142f12701d435a64b63055fe02c8349d0edcb
 
 namespace file_tools
 {
